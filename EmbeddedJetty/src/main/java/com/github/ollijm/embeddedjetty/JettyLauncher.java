d440c0266e23b8acb6f5f9dc1ef0cf861a8225e5
Olli Miettinen <ollijm@gmail.com>
2011-09-18 19:43:03 +0300

    add empty main method

---
+++ EmbeddedJetty/src/main/java/com/github/ollijm/embeddedjetty/JettyLauncher.java
@@ -0,0 +1,14 @@
+package com.github.ollijm.embeddedjetty;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: olli
+ * Date: 9/18/11
+ * Time: 7:42 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class JettyLauncher {
+    public static void main(String[] args) {
+
+    }
+}
